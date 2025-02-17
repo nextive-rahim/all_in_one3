af25ec953b26044ac0c3605bb2da58a46c02c9de
Abdul Rahim <113752743+nextive-rahim@users.noreply.github.com>
2025-02-18 03:09:17 +0600

    chore : Updated delete job icon

--- lib/src/features/company_module/mobile/company_job/my_company_jobs/my_company_job_list/widget/delete_company_job.dart
+++ lib/src/features/company_module/mobile/company_job/my_company_jobs/my_company_job_list/widget/delete_company_job.dart
@@ -1,7 +1,8 @@
-import 'package:all_in_one3/src/core/utils/colors.dart';
+import 'package:all_in_one3/src/core/theme/colors.dart';
 import 'package:all_in_one3/src/core/utils/image_constant.dart';
 import 'package:all_in_one3/src/core/utils/strings.dart';
 import 'package:all_in_one3/src/core/utils/util.dart';
+import 'package:all_in_one3/src/core/widgets/primary_button.dart';
 import 'package:all_in_one3/src/features/company_module/mobile/company_job/my_company_jobs/my_company_job_list/controller/company_job_view_controller.dart';
 import 'package:all_in_one3/src/features/student_module/mobile/job/jobs/model/view_job_model.dart';
 import 'package:flutter/material.dart';
@@ -14,7 +15,7 @@ class DeletedCompanyJob extends GetView<CompanyJobViewController> {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        _showDailogBox(context);
+        _showError(context, 'Deleted job is irreversible.');
       },
       child: Row(
         children: [
@@ -35,37 +36,70 @@ class DeletedCompanyJob extends GetView<CompanyJobViewController> {
     );
   }
 
-  void _showDailogBox(BuildContext context) {
+  void _showError(BuildContext context, String message, {String? title}) {
     showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
-          title: const Text('Do you want delete this job?'),
-          content: const SingleChildScrollView(
-            child: SelectableText('Delete is irreversible.'),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                controller.deleteCompanyJob(job.id!).then((v) {
-                  controller.companyJobList.removeWhere((v) => v.id == job.id);
-                  SnackBarService.showInfoSnackBar('Successfully delete job.');
-                });
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title ?? 'Do you want delete this job?',
+                style: TextStyle(
+                  color: Color(0xFF363636),
+                  fontSize: 18,
 
-                Navigator.of(context).pop();
-              },
-              child: const Text(
-                'OK',
-                style: TextStyle(color: CommonColor.redColors),
+                  fontWeight: FontWeight.w700,
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: 15),
+              Divider(),
+              Text(
+                message,
+                style: TextStyle(
+                  color: Color(0xFF8A8A8A),
+                  fontSize: 12,
+
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              SizedBox(height: 25),
+              PrimaryButton(
+                onTap: () {
+                  controller.deleteCompanyJob(job.id!).then((v) {
+                    controller.companyJobList.removeWhere(
+                      (v) => v.id == job.id,
+                    );
+                    SnackBarService.showInfoSnackBar(
+                      'Successfully delete job.',
+                    );
+                  });
+
+                  Navigator.of(context).pop();
+                },
+                title: 'Yes, Delete',
+              ),
+              SizedBox(height: 12),
+              PrimaryButton(
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+                widget: Text(
+                  'Cancel',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w500,
+                    // height: 1.56,
+                  ),
+                ),
+
+                backgroundColor: AppColors.transparent,
+              ),
+            ],
+          ),
         );
       },
     );
